c10cd5bb18864ff1bb9836f90ebff3fb8a29bf65
Jim <jimmycn03@gmail.com>
2024-06-15 16:59:35 +0300

    Added new requirements and opening

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
-module main.hardware_pos_v2 {
+module main.hardware_pos_v {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires java.naming;
 
 
     opens main.hardware_pos_v2 to javafx.fxml;
+    opens main.hardware_pos_v2.Database.Entity to org.hibernate.orm.core;
     exports main.hardware_pos_v2;
 }
