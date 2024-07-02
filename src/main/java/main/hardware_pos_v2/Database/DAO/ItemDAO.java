871bc370b3997107b93a706970c59b6a4d521199
Jim <jimmycn03@gmail.com>
2024-07-02 16:19:27 +0300

    Added methods to complete CRUD operations

--- src/main/java/main/hardware_pos_v2/Database/DAO/ItemDAO.java
+++ src/main/java/main/hardware_pos_v2/Database/DAO/ItemDAO.java
@@ -33,8 +33,8 @@ public class ItemDAO {
         try (Session session = sessionFactory.openSession()) {
             transaction = session.beginTransaction();
             // Check if the specified category exists
-            Query<Category> query = session.createQuery("SELECT FROM categories WHERE name = :categoryName", Category.class);
-            query.setParameter("itemName", newItem.getCategoryName());
+            Query<Category> query = session.createQuery("FROM Category WHERE name = :categoryName", Category.class);
+            query.setParameter("categoryName", newItem.getCategoryName());
             Category itemCategory = query.uniqueResult();
             // If category doesn't exist
             if (itemCategory == null) {
@@ -47,6 +47,7 @@ public class ItemDAO {
             databaseItem.setBuyingPrice(newItem.getBuyingPrice());
             databaseItem.setSellingPrice(newItem.getSellingPrice());
             databaseItem.setCategory(itemCategory);
+            databaseItem.setStock(newItem.getQuantity());
 
             // Save the object
             session.save(databaseItem);
@@ -62,4 +63,176 @@ public class ItemDAO {
         }
     }
 
+    /**
+     * retrieves an item from the database
+     * @param itemName the name of the item to retrieve
+     * @return true/false mapped with result string
+     */
+    public Map<Boolean, String> readItem(String itemName){
+        Transaction transaction = null;
+        Map<Boolean, String> result = new HashMap<>();
+        try (Session session =sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            Query<DatabaseItem> query = session.createQuery("FROM DatabaseItem WHERE name = :itemName", DatabaseItem.class);
+            query.setParameter("itemName", itemName);
+            DatabaseItem item = query.uniqueResult();
+            // Check if item exists
+            if (item == null) {
+                result.put(false, "Item not found");
+                return result;
+            }
+            // Return the item
+            result.put(true, item.toString());
+            return result;
+        } catch (Exception e) { // In case of any errors
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            result.put(false, "An error occurred: " + e.getMessage());
+            return result;
+        }
+    }
+
+    /**
+     * updates an item in the database
+     * @param newItemData the new data to update to
+     * @param currentItemName the name of the item to update
+     * @return map of true/false with message
+     */
+    public Map<Boolean, String> updateItem(Item newItemData, String currentItemName){
+        Map<Boolean, String> result = new HashMap<>();
+        Transaction transaction = null;
+        // Check if a name was provided
+        if (newItemData == null) {
+            result.put(false, "Item data can't be empty");
+            return result;
+        } else {
+            try (Session session = sessionFactory.openSession()) {
+                transaction = session.beginTransaction();
+                // Fetch the outdated data
+                Query<DatabaseItem> query = session.createQuery("FROM DatabaseItem WHERE name=:itemName", DatabaseItem.class);
+                query.setParameter("itemName", currentItemName);
+                DatabaseItem oldItemData = query.uniqueResult();
+                // If data isn't found
+                if (oldItemData == null) {
+                    result.put(false, "Item data not found");
+                    return result;
+                }
+                // Check if the new category exists
+                Query<Category> categoryQuery = session.createQuery("FROM Category WHERE name = :categoryName", Category.class);
+                categoryQuery.setParameter("categoryName", newItemData.getCategoryName());
+                Category itemCategory = categoryQuery.uniqueResult();
+                // If category doesn't exist
+                if (itemCategory == null) {
+                    result.put(false, "The specified category wasn't found");
+                    return result;
+                }
+                // Update to the new data
+                oldItemData.setName(newItemData.getName());
+                oldItemData.setCategory(itemCategory);
+                oldItemData.setBuyingPrice(newItemData.getBuyingPrice());
+                oldItemData.setSellingPrice(newItemData.getSellingPrice());
+                oldItemData.setStock(newItemData.getQuantity());
+                // Save the new data
+                session.saveOrUpdate(oldItemData);
+                transaction.commit();
+                result.put(true, "Item's data has been successfully added");
+                return result;
+            } catch (Exception e) { // In case  of errors
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                result.put(false, e.getMessage());
+                return result;
+            }
+        }
+    }
+
+    /**
+     * deletes an item from the database
+     * @param itemName the name of the item to delete
+     * @return map of true/false and a message
+     */
+    public Map<Boolean, String> deleteItem(String itemName) {
+        Map<Boolean, String> result = new HashMap<>();
+        Transaction transaction = null;
+        // Check if name was provided
+        if (itemName == null) {
+            result.put(false, "Item name cant be empty");
+            return result;
+        }
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            // Fetch the item's data
+            Query<DatabaseItem> query = session.createQuery("FROM DatabaseItem WHERE name= :itemName", DatabaseItem.class);
+            query.setParameter("itemName", itemName);
+            DatabaseItem item = query.uniqueResult();
+            // Data not found
+            if (item == null) {
+                result.put(false, "Specified item not found");
+            } else {
+                // Delete the item
+                session.delete(item);
+                transaction.commit();
+                result.put(true, "Item successfully deleted");
+            }
+            return result;
+        } catch (Exception e) { // In case of errors
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            result.put(false, e.getMessage());
+            return result;
+        }
+    }
+
+    /**
+     * reduces the stock of a specific item
+     * @param itemName the name of the item
+     * @param quantity the amount to reduce from the stock
+     * @return map of true/false with a message
+     */
+    public Map<Boolean, String> reduceStock(String itemName, int quantity){
+        Map<Boolean, String> result = new HashMap<>();
+        Transaction transaction = null;
+        // Check if item name was specified
+        if (itemName == null) {
+            result.put(false, "Item's name can't be empty");
+            return result;
+        } else if (quantity <= 0) { // Check if the amount to reduce is reasonable
+            result.put(false, "The quantity should be above 0");
+            return result;
+        } else {
+            try (Session session = sessionFactory.openSession()) {
+                transaction = session.beginTransaction();
+                // Fetch the item's data
+                Query<DatabaseItem> query = session.createQuery("FROM DatabaseItem WHERE name = :itemName", DatabaseItem.class);
+                query.setParameter("itemName", itemName);
+                DatabaseItem item = query.uniqueResult();
+                // Item not found
+                if (item == null) {
+                    result.put(false, "Item not found in database");
+                    return result;
+                } else if (quantity > item.getStock()) { // Result in negative stock count
+                    result.put(false, "The entered quantity is more than the available stock" +
+                            "Number of items = " + quantity +
+                            "Remaining stock =" + item.getStock());
+                    return result;
+                }
+                // Reduce the stock
+                item.setStock(item.getStock() - quantity);
+                // Update the new stock size
+                session.saveOrUpdate(item);
+                transaction.commit();
+                result.put(true, "Item stock reduced");
+                return result;
+            } catch (Exception e) { // In case of errors
+                if (transaction != null) {
+                    transaction.rollback();
+                }
+                result.put(false, "An error occured: " + e.getMessage());
+                return result;
+            }
+        }
+    }
 }
