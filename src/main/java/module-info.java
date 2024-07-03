17917cb61281ff2b3008da748be7a396016b545f
Jim <jimmycn03@gmail.com>
2024-07-03 15:46:40 +0300

    Changed to the correct project name and added a new opening

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,4 @@
-module main.hardware_pos_v {
+module main.hardware_pos_v2 {
     requires javafx.controls;
     requires javafx.fxml;
     requires jakarta.persistence;
@@ -8,6 +8,7 @@ module main.hardware_pos_v {
 
 
     opens main.hardware_pos_v2 to javafx.fxml;
+    opens main.hardware_pos_v2.Controllers to javafx.fxml;
     opens main.hardware_pos_v2.Database.Entity to org.hibernate.orm.core;
     exports main.hardware_pos_v2;
 }
