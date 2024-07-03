ba9a3e7e1b09268a95a84fcb9b64fe3967b87d4e
Jim <jimmycn03@gmail.com>
2024-07-03 09:55:52 +0300

    Renamed the file to Main

--- src/main/java/main/hardware_pos_v2/Main.java
+++ src/main/java/main/hardware_pos_v2/Main.java
@@ -7,13 +7,14 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("POS");
         stage.setScene(scene);
+        stage.setFullScreen(true);
         stage.show();
     }
 
