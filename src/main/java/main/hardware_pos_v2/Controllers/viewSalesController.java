358b9cfae00d6bfd5469f83ffabc927361a6b4d0
Jim <jimmycn03@gmail.com>
2024-07-10 10:31:48 +0300

    Added functionality to accommodate the new table view

--- src/main/java/main/hardware_pos_v2/Controllers/viewSalesController.java
+++ src/main/java/main/hardware_pos_v2/Controllers/viewSalesController.java
@@ -1,10 +1,13 @@
 package main.hardware_pos_v2.Controllers;
 
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import main.hardware_pos_v2.Database.DAO.SalesDAO;
 import main.hardware_pos_v2.Database.Entity.Sale;
@@ -14,26 +17,44 @@ import java.util.List;
 public class viewSalesController {
 
     @FXML
-    private ListView<String> listView;
+    private TableColumn<Sale, String> customerNameColumn;
+
+    @FXML
+    private TableColumn<Sale, String> customerPhoneColumn;
+
+    @FXML
+    private TableColumn<Sale, Double> debtColumn;
+
+    @FXML
+    private TableColumn<Sale, String> itemsColumn;
+
+    @FXML
+    private TableView<Sale> salesTable;
 
     @FXML
     private ScrollPane scrollPane;
 
     @FXML
+    private TableColumn<Sale, Double> totalPaidColumn;
+
+    @FXML
     private VBox vbox;
 
     public void initialize() {
-        listView.prefWidthProperty().bind(vbox.widthProperty());
+        customerNameColumn.setCellValueFactory(sale -> new SimpleStringProperty(sale.getValue().getCustomerName()));
+        customerPhoneColumn.setCellValueFactory((phone -> new SimpleStringProperty(phone.getValue().getPhoneNumber())));
+        itemsColumn.setCellValueFactory(items -> new SimpleStringProperty(items.getValue().getItemList()));
+        totalPaidColumn.setCellValueFactory(payment -> new SimpleDoubleProperty(payment.getValue().getListTotal()).asObject());
+        debtColumn.setCellValueFactory(debt -> new SimpleDoubleProperty(debt.getValue().getDebt()).asObject());
+        salesTable.prefWidthProperty().bind(vbox.widthProperty());
         fetchAll();
     }
 
-    public void fetchAll() {
+    private void fetchAll() {
+        ObservableList<Sale> saleObservableList = FXCollections.observableArrayList();
         SalesDAO salesDAO = new SalesDAO();
-        List<Sale> sales = salesDAO.getAll();
-        ObservableList<String> list = FXCollections.observableArrayList();
-        for (Sale sale: sales) {
-            list.add(sale.toString());
-        }
-        listView.setItems(list);
+        List<Sale> list = salesDAO.getAll();
+        saleObservableList.addAll(list);
+        salesTable.setItems(saleObservableList);
     }
 }
