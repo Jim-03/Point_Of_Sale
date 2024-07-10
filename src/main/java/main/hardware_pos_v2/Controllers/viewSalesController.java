fe43841d5865282c4bc241ca7665128b93a60dc3
Jim <jimmycn03@gmail.com>
2024-07-10 12:52:13 +0300

    Added more fields for output

--- src/main/java/main/hardware_pos_v2/Controllers/viewSalesController.java
+++ src/main/java/main/hardware_pos_v2/Controllers/viewSalesController.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.VBox;
 import main.hardware_pos_v2.Database.DAO.SalesDAO;
 import main.hardware_pos_v2.Database.Entity.Sale;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 public class viewSalesController {
@@ -23,7 +24,7 @@ public class viewSalesController {
     private TableColumn<Sale, String> customerPhoneColumn;
 
     @FXML
-    private TableColumn<Sale, Double> debtColumn;
+    private TableColumn<Sale, Double> changeColumn;
 
     @FXML
     private TableColumn<Sale, String> itemsColumn;
@@ -38,14 +39,28 @@ public class viewSalesController {
     private TableColumn<Sale, Double> totalPaidColumn;
 
     @FXML
+    private TableColumn<Sale, String> dateColumn;
+
+    @FXML
+    private TableColumn<Sale, String> servedByColumn;
+
+    @FXML
+    private TableColumn<Sale, Double> totalAmountColumn;
+    @FXML
     private VBox vbox;
 
     public void initialize() {
         customerNameColumn.setCellValueFactory(sale -> new SimpleStringProperty(sale.getValue().getCustomerName()));
         customerPhoneColumn.setCellValueFactory((phone -> new SimpleStringProperty(phone.getValue().getPhoneNumber())));
         itemsColumn.setCellValueFactory(items -> new SimpleStringProperty(items.getValue().getItemList()));
-        totalPaidColumn.setCellValueFactory(payment -> new SimpleDoubleProperty(payment.getValue().getListTotal()).asObject());
-        debtColumn.setCellValueFactory(debt -> new SimpleDoubleProperty(debt.getValue().getDebt()).asObject());
+        totalAmountColumn.setCellValueFactory(total -> new SimpleDoubleProperty(total.getValue().getListTotal()).asObject());
+        totalPaidColumn.setCellValueFactory(payment -> new SimpleDoubleProperty(payment.getValue().getTotalPaid()).asObject());
+        changeColumn.setCellValueFactory(debt -> new SimpleDoubleProperty(debt.getValue().getDebt()).asObject());
+        dateColumn.setCellValueFactory(date -> {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+            return new SimpleStringProperty(date.getValue().getDate().format(formatter));
+        });
+        servedByColumn.setCellValueFactory(server -> new SimpleStringProperty(server.getValue().getServerName()));
         salesTable.prefWidthProperty().bind(vbox.widthProperty());
         fetchAll();
     }
